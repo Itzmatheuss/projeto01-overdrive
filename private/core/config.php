f014537c9104ff04055b436939d2fa5b428ede02
itzmatheuss <matheusp.ss199@gmail.com>
2023-11-13 18:08:46 -0300

    front_login

---
+++ private/core/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DBNAME', 'projeto01-overdrive');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+
+define('ROOT','http://localhost/estagiopoo/projeto01-overdrive/public');
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/ecology.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  
+</head>
+<body>
+    <div class="img"></div>
+    <main class="wrapper">
+        <div class="title">
+            <span>Login Form</span>
+        </div>
+        <form action="#">
+            <div class="row">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+            <input type="text" placeholder="Insira seu CPF" required>
+            </div>
+            <div class="row">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
+            <input type="text" placeholder="Insira sua senha" required>
+            </div>
+            <div class="row">
+            <input type="submit" value="Entrar">
+            </div>
+
+        </form>
+    </main>
+</body>
+</html>
