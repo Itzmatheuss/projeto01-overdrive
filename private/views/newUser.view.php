fc598117be777feb50bd6a99506a4c707e85235d
itzmatheuss <matheusp.ss199@gmail.com>
2023-12-14 13:52:50 -0300

    portugues

--- private/views/newUser.view.php
+++ private/views/newUser.view.php
@@ -12,7 +12,7 @@ $result = $conn->viewEmpresas();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novo Usuários</title>
+    <title>Novo Usuário</title>
     <link rel="shortcut icon" href="ecology.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/newUserstyle.css">
     <script src="../bootstrap/js/jquery-3.7.1.min.js"></script>
--- public/newUserComum.php
+++ public/newUserComum.php
@@ -11,8 +11,8 @@ $result = $conn->viewEmpresas();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novo Usuários</title>
-    <link rel="shortcut icon" href="ecology.png" type="image/x-icon">
+    <title>Novo Usuário</title>
+    <link rel="shortcut icon" href="images/ecology.png" type="image/x-icon">
     <link rel="stylesheet" href="css/newUserstyle.css">
     <link rel="stylesheet" href="./css/box.css">
     <script src="js/jquery-3.7.1.min.js"></script>
